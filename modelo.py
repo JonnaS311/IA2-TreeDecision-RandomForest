4d433bde2f0117d0d57d98c3c8cba46bfbeaf360
Jonanthan Sotelo <jsotelor@udistrital.edu.co>
2025-01-22 16:13:17 -0500

    random foresttt

--- modelo.py
+++ modelo.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report
 import matplotlib.pyplot as plt
 
@@ -41,6 +42,30 @@ def train_decision_tree(X, y, max_depth=5, random_state=42):
     
     return dt, X_train, X_test, y_test, y_pred, accuracy, report
 
+# Entrenamiento y evaluación del modelo
+def train_random_forest(X, y, n_estimators=100, max_depth=None, random_state=42):
+    # Dividir datos en entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=random_state
+    )
+    
+    # Crear y entrenar el modelo
+    rf = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=random_state
+    )
+    rf.fit(X_train, y_train)
+    
+    # Hacer predicciones
+    y_pred = rf.predict(X_test)
+    
+    # Evaluar el modelo
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred, zero_division=1)
+    
+    return rf, X_train, X_test, y_test, y_pred, accuracy, report
+
 # Visualizar el árbol
 def plot_decision_tree(dt, feature_names, class_names=None, figsize=(20,10)):
     plt.figure(figsize=figsize)
@@ -62,8 +87,9 @@ df = pd.read_csv('preprocesado.csv')
 # Preparar datos
 X, y = prepare_data(df,target_column,feature_columns)
 
-# Entrenamineto y reporte del modelo
-dt, X_train, X_test, y_test, y_pred, accuracy, report = train_decision_tree(X, y)
+# Entrenamiento y reporte del modelo
+dt, X_train, X_test, y_test, y_pred, accuracy, report = train_random_forest(X, y)
+#dt, X_train, X_test, y_test, y_pred, accuracy, report = train_decision_tree(X, y)
 
 # Imprimir resultados
 print(f'Accuracy: {accuracy}')
@@ -71,4 +97,4 @@ print('\nClassification Report:')
 print(report)
 
 # Visualizar árbol
-plot_decision_tree(dt, feature_names=feature_columns)
+#plot_decision_tree(dt, feature_names=feature_columns)
